f673dff75219884c58d84ebd1f641ff90ec3779b
kermitfrog <kermit@ag.de1.cc>
2015-04-07 02:04:58 +0200

    mare packaging stuff

--- scripts/make_udev_rules.sh
+++ scripts/make_udev_rules.sh
@@ -9,7 +9,7 @@ else
 	GRP=$1
 fi
 
-FILE="99-inputmangler.rules"
+FILE="45-inputmangler.rules"
 
 sed --expression "s/Vendor=0000/Vendor=x/" /proc/bus/input/devices | grep --extended-regexp "^I:.*Vendor=[0-9]" --after-context=1 | awk '/^I:.*/{x=$0;next}/^N:.*/{print $0"\n"x;}/^--.*/{print}' - > $FILE
 sed --in-place --expression "s/--//" $FILE
@@ -20,8 +20,8 @@ sed --in-place --expression "s/ Version=.*/\", MODE=\"0660\", GROUP=\"$GRP\" RUN
 chmod 644 $FILE
 
 echo ""
-echo "Delete the comment (#) sign of the appropriate lines (starting with ATTRS{idVendor}) in 99-inputmangler.rules to enable the Devices you want to use with inputmangler. Then execute the following line to install the file in /etc/udev/rules.d/"
-echo "sudo cp 99-inputmangler.rules /etc/udev/rules.d/ ; sudo chmod 644 /etc/udev/rules.d/99-inputmangler.rules"
+echo "Delete the comment (#) sign of the appropriate lines (starting with ATTRS{idVendor}) in $FILE to enable the Devices you want to use with inputmangler. Then execute the following line to install the file in /etc/udev/rules.d/"
+echo "sudo cp $FILE /etc/udev/rules.d/ ; sudo chmod 644 /etc/udev/rules.d/99-inputmangler.rules"
 echo "" 
 echo "Please note, that udev does not set the permissions for PS/2 devices. See the README, section ##INS## on how to do that."
 echo ""
